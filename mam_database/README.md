03c52ad44d75964d4e1a1c2d8351f240d644518e
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-10-07 10:18:41 +0200

    new files insert

--- mam_database/README.md
+++ mam_database/README.md
@@ -1,3 +1,6 @@
-# mam_database
-
-A new Flutter project.
+# MAM_Database
+Version 3 nach Übertrag
+Mock Database implementiert
+Database repository angepasst / Methoden erstellt und getestet
+##
+Methode zum Login wurde hinzugefügt und die Methoden zum einfügen von Daten in die Listen hinzugefügt
--- mam_database/lib/main.dart
+++ mam_database/lib/main.dart
@@ -1,20 +1,64 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: unused_import, unused_local_variable
+
+import "dart:io";
+import "models/trash_abc.dart";
+import "models/user.dart";
+import "data/database_repository.dart";
+import "data/mock_database.dart";
+import "functions/caseabcswitch.dart";
+
+import "models/cancolor.dart";
+import "models/news.dart";
 
 void main() {
-  runApp(const MainApp());
-}
+  DatabaseRepository databaseRepository = MockDatabase();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  Trashabc obj =
+      Trashabc(name: "name", explanation: "Test", picture: "picture");
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  print(obj.explanation);
+
+  // Hier werden die Methoden zum adden aufgerufen
+  // später werden noch die Variablen surch inputs ersetzt
+  databaseRepository.addNewcanColor("color", "text", "picture");
+  databaseRepository.addNewNews(
+      "newstitle", "newsdate", "newstext", "newspicture");
+  databaseRepository.addNewTrashInformation(
+      "Testname", "Testbezeichnung", "Testbild");
+  databaseRepository.addNewTrashInformation(
+      "Testname", "Testbezeichnung", "Testbild");
+
+// add new User
+
+// hier wird die remove Methode getestet fuktioniert noch nicht
+
+  databaseRepository.removeTrashInformation(
+      "Testname", "Testbezeichnung", "Testbild");
+
+// Mit den Prints wird getestet ob die Add Methoden fukktionieren
+
+  print(databaseRepository.getAllNews());
+  print(databaseRepository.getAllcans());
+  print(databaseRepository.getAllTrashInformation());
+
+// Hier startet die App
+
+  stdout.writeln(" Herzlich Willkomen zur Marler Müll App");
+
+  stdout.writeln(" Sie haben die Wahl. Was möchten Sie tun? \n [L]ogin");
+  String? choice = stdin.readLineSync();
+
+  switch (choice) {
+    // Hier wird die Login Methode aufgerufen aus der Database
+    case "L":
+      stdout.writeln("Bitte geben Sie ihren Usernamen ein");
+      String? username = stdin.readLineSync();
+      stdout.writeln("Bitte geben Sie ihr Passwort ein");
+      String? userpassword = stdin.readLineSync();
+      if (databaseRepository.loggedincorrectly(username!, userpassword!) ==
+          true) {
+        print("jetzt gehts weiter");
+      }
   }
+  // Hier entsteht ein neuer Case für das registrieren
 }
